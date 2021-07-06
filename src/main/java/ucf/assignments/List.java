017ea3d42a9a6b224670a68ed69b2a833da6fac7
agalvezv <43121059+agalvezv@users.noreply.github.com>
2021-07-06 18:58:50 -0400

    Updates to build grade, interface and future scene.

---
+++ src/main/java/ucf/assignments/List.java
@@ -0,0 +1,31 @@
+package ucf.assignments;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class List extends Application {
+
+
+
+    //This will be for the launching of the UI
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Interface.fxml"));
+
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("List Operations");
+        primaryStage.show();
+    }
+}
