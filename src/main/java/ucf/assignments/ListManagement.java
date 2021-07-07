a7a215e23c9251c4152f5f3ad027e84405391a65
agalvezv <43121059+agalvezv@users.noreply.github.com>
2021-07-06 23:43:15 -0400

    Updating ListManagement pseudocode.

--- src/main/java/ucf/assignments/ListManagement.java
+++ src/main/java/ucf/assignments/ListManagement.java
@@ -12,6 +12,7 @@ public class ListManagement {
     //description
     //due date
     //bool to mark complete part of a list
+    //dominating list of lists that represent the total package of items and info
 
     //Strings to intake information received from interface
     //constructor to receive in bulk
@@ -19,17 +20,31 @@ public class ListManagement {
 
     //get and setter for each array list
 
+    //get set in the vein of this:
+    /*
+        public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+     */
+
     //function to add this into array list
     //this can be implemented simply with using the add functionality
+    //this add function will represent the add operations of multiple lists that are part of the title, description and so on.
 
     //function to sort list
     //sort will be a private functino and used internally in list management
     //most likely I will bring in java.util.Collections
     //and sort using its sort option for the titles and
-    //using a loop to adjust description and due date
+    //using a loop to adjust description and due date in conjunction with the alphabetically arranged titles
 
     //function to remove from the array list
     //this will be implemented with the remove function that is part of the array list
+    //this add function will represent the remove operations of multiple lists that are part of the title, description and so on.
+    //obviously as an item is taken from the list everything else must be removed
 
     //function edit array lists
     //loop to parse through and find a match for what needs to change,
@@ -47,13 +62,9 @@ public class ListManagement {
     //adjust other array lists according with loop method
 
     //function to print completed parts of list
-    //loop to print out completed items with the bool paramater as a check
+    //loop to print out completed items with the bool parameter as a check
 
     //function to print non-completed parts of list
-    //loop to print out non-completed items with the bool paramater as a check
+    //loop to print out non-completed items with the bool parameter as a check
 
-
-
-
-    //function to remove from
 }
