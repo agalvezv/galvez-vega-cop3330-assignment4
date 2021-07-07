d18b7732dcbbd061e2da70c5bfe9681658e2bb63
agalvezv <43121059+agalvezv@users.noreply.github.com>
2021-07-06 23:38:41 -0400

    Updated ListMoveOperations pseudocode.

--- src/main/java/ucf/assignments/ListMoveOperations.java
+++ src/main/java/ucf/assignments/ListMoveOperations.java
@@ -14,12 +14,19 @@ public class ListMoveOperations {
 
     //function that will function as the read file
     //uses a try catch to absorb specific error
+    //while loop inside try catch in conjunction with scanner to take in info
+    //catch prints out an error just in case
     //io.File, io.FileWriter will be in use
     //read function will specifically read in from an external storage array and
     //recreate lists that will be returned to inferface
 
     //function that will function as the writer area for one list
     //also uses a try catch to absorb potential errors
+    //FileWriter writeFile that grabs from the source path where things will be written
+    //information written to this output file
+    //file close
+
+    //print an exception if things don't go accordingly
     //this will /take in/ a set of array lists and send them to a specific external storage area
 
 
